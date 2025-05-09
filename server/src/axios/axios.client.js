0208d6165b41c160f4bf6a7e954d31b73ecf7b4c
kumudu-dodandeniya <69890196+kumudu-dodandeniya@users.noreply.github.com>
2025-05-09 19:49:56 +0530

    create movie explore app

---
+++ server/src/axios/axios.client.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const get = async (url) => {
+  const response = await axios.get(url);
+/*
+  const response = await axios.get(url, {
+    headers: {
+      Accept: "application/json",
+      "Accept-Encoding": "identity"
+    }
+  });
+*/
+  return response.data;
+};
+
+export default { get };
